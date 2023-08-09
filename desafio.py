5503cd82aa557583f7929b7c5879c10a29bbf962
Cesar Freire <sezaruuufreire@outlook.com>
2023-08-08 23:47:18 -0300

    Atualizando aplicado regra de negocio

--- desafio.py
+++ desafio.py
@@ -17,18 +17,56 @@ while True:
 
     opcao = input(menu)
 
-    if opcao == "d":
-        print("Deposito")
+    if opcao == "1":
+        print()
+        valor = float(input("Informe o valor do depositos: "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Deposito: R$ {valor:.2f}\n"
+
+        else:
+            print("Operação falhou! o valor informado é inválido.")
+        
+    elif opcao == "2":
+        valor = float(input("Informe o valor do saque: "))
+        
+        excedeu_saldo = valor > saldo
+        excedeu_limite = valor > limite
+        excedeu_saques = numero_saque >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("Operação falhou! Você não tem saldo sufuciente.")
+
+        elif excedeu_limite:
+            print("Operação falhou! O valor do saque excede o limite.")
+
+        elif excedeu_saques:
+            print("Operação falhou! Número máximo de saques excedido.")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            numero_saque += 1
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+        
+    elif opcao == "3":
+        print("\n=============== EXTRATO ===============")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("========================================")
+
+
         
     elif opcao == "s":
-        print("Saque")
-        
-    elif opcao == "e":
-        print("Extrato")
-        
-    elif opcao == "q":
-        print("Sair")
+        print()
+        print("Saindo do programa")
+        print("========================================")
+        print()
         break
 
     else:
+        print()
         print("Operação inválida, por favor selecionar a operação desejada.")
